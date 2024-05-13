c28a69ebd91fa2c6932b830b1bd2cd45ae65ef53
Frederico Ribeiro <frederico.ribeiro@fiscal.io>
2024-05-13 15:22:59 -0300

    feat:nova função que pega os valores do Cadastrar.html

--- frontend/scripts/Cadastrar.js
+++ frontend/scripts/Cadastrar.js
@@ -1,13 +1,38 @@
 const btnLinha = document.getElementById("AdicionarLinha");
+const btnSalvar = document.getElementById("btn-salvar");
 const table = document.getElementById("table");
+let numberoLinhas = 1;
 btnLinha.addEventListener("click", () => {
   row = "<td>";
   row =
     row +
-    '<input type="text" /></td> <td><input type="text" /></td> <td><input type="text" /></td> <td><button class="btn-excluir">Excluir</button></td>';
+    `<input id="idItem${numberoLinhas}"  type="text" /></td> <td><input id="Produto${numberoLinhas}" type="text" /></td> <td><input id="Valor${numberoLinhas}" type="text" /></td> <td><button class="btn-excluir">Excluir</button></td>`;
   row = row + "</td>";
   const novaLinha = document.createElement("tr");
   novaLinha.innerHTML = row;
-  console.log(novaLinha.innerHTML);
+  novaLinha.id = numberoLinhas;
   table.insertAdjacentElement("beforeend", novaLinha);
+  numberoLinhas++;
+});
+btnSalvar.addEventListener("click", () => {
+  const itens = [];
+  const idNota = document.getElementById("idNota");
+  const Emissor = document.getElementById("Emissor");
+  const Data = document.getElementById("Data");
+  for (let i = 1; i < numberoLinhas; i++) {
+    const idItem = document.getElementById(`idItem${i}`);
+    const Produto = document.getElementById(`Produto${i}`);
+    const Valor = document.getElementById(`Valor${i}`);
+    console.log(idItem.value, Produto.value, Valor.value);
+    itens.push([idItem.value, Produto.value, parseFloat(Valor.value)]);
+  }
+  console.log(idNota.value);
+  const dados = {
+    idNota: idNota.value,
+    Emissor: Emissor.value,
+    Data: Data.value,
+    Itens: itens,
+  };
+  console.log(itens);
+  console.log(dados);
 });
